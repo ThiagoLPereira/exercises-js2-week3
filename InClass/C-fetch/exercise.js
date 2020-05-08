d96cecd4dfbba85cce5051cec6da358d983686e1
ThiagoLPereira <t.luizper@gmail.com>
2020-05-08 18:21:25 +0200

    week9_inClass-C-fetch

--- InClass/C-fetch/exercise.js
+++ InClass/C-fetch/exercise.js
@@ -10,3 +10,20 @@ When you get the response from the server, print the current temperature in an <
 
 ================
 */
+
+fetch("https://fcc-weather-api.glitch.me/api/current?lat=35&lon=160")
+  .then(function (response) {
+    var myJson = response.json();
+  })
+  .then(function (myJson) {
+    console.log(myJson.main.temp);
+  })
+  .catch(function (error) {
+    console.log("There was an error");
+  });
+
+var lat = 35;
+var lon = 160;
+var url = "https://fcc-weather-api.glitch.me/api/current";
+url += "?lat=" + lat;
+url += "&lon=" + lon;
