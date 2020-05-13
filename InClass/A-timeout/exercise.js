2bc19236c716916cb3bacddf89f2a6c27dca9016
ThiagoLPereira <t.luizper@gmail.com>
2020-05-13 15:35:30 +0200

    week9_inClass-A-timeout

--- InClass/A-timeout/exercise.js
+++ InClass/A-timeout/exercise.js
@@ -9,3 +9,16 @@ Task 2
 Update your code to make the colour change every 5 seconds to something different. Hint: try searching for setInterval. (https://www.w3schools.com/jsref/met_win_setinterval.asp)
 ================
 */
+
+var colors = ["white", "lightblue", "lightpink", "red", "yellow"];
+var colorIndex = 0;
+document.body.style.backgroundColor = colors[colorIndex];
+
+setInterval(changeBgColor, 5000);
+function changeBgColor() {
+  colorIndex++;
+  if (colorIndex == colors.length) {
+    colorIndex = 0;
+  }
+  document.body.style.backgroundColor = colors[colorIndex];
+}
