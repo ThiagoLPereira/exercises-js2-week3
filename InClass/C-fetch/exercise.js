c9c81ed0650dc6c4a0b9a98f9845252e740d24f4
ThiagoLPereira <t.luizper@gmail.com>
2020-05-13 15:29:48 +0200

    week9_inClass-C-fetch

--- InClass/C-fetch/exercise.js
+++ InClass/C-fetch/exercise.js
@@ -11,19 +11,43 @@ When you get the response from the server, print the current temperature in an <
 ================
 */
 
-fetch("https://fcc-weather-api.glitch.me/api/current?lat=35&lon=160")
-  .then(function (response) {
-    var myJson = response.json();
-  })
-  .then(function (myJson) {
-    console.log(myJson.main.temp);
-  })
-  .catch(function (error) {
-    console.log("There was an error");
-  });
+var getButton = document.querySelector("#getButton");
+getButton.addEventListener("click", getWeatherData);
 
-var lat = 35;
-var lon = 160;
-var url = "https://fcc-weather-api.glitch.me/api/current";
-url += "?lat=" + lat;
-url += "&lon=" + lon;
+function getWeatherData(event) {
+  var latitudeInput = document.querySelector("#latitude");
+  var latitude = latitudeInput.value;
+
+  var longitudeInput = document.querySelector("#longitude");
+  var longitude = longitudeInput.value;
+
+  fetchWeatherData(latitude, longitude);
+  event.preventDefault();
+}
+
+function fetchWeatherData(latitude, longitude) {
+  var url = generateWeatherUrl(latitude, longitude);
+
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (responseJson) {
+      var temp = responseJson.main.temp;
+      showTemperature(temp);
+    });
+}
+
+function generateWeatherUrl(latitude, longitude) {
+  var url = "https://fcc-weather-api.glitch.me/api/current";
+  url += "?lat=" + latitude;
+  url += "&lon=" + longitude;
+  return url;
+}
+
+function showTemperature(temperature) {
+  var mainDiv = document.querySelector("#main");
+  var tempHeader = document.createElement("h3");
+  mainDiv.appendChild(tempHeader);
+  tempHeader.innerText = temperature;
+}
