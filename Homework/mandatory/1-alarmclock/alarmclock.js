c9fa9394590f90f1576300076a98f441229bf014
ThiagoLPereira <t.luizper@gmail.com>
2020-05-13 15:38:27 +0200

    week9_homeWork-1-alarmClock

--- Homework/mandatory/1-alarmclock/alarmclock.js
+++ Homework/mandatory/1-alarmclock/alarmclock.js
@@ -1,4 +1,46 @@
-function setAlarm() {}
+var alarmSeconds;
+var alarmInterval;
+var millisPerSecond = 1000;
+
+function setAlarm() {
+  var alarmInput = document.querySelector("#alarmSet");
+  alarmSeconds = alarmInput.value;
+  alarmInterval = setInterval(subtractAlarmSecond, millisPerSecond);
+  displayTimeRemaining();
+}
+
+function subtractAlarmSecond() {
+  alarmSeconds = alarmSeconds - 1;
+  displayTimeRemaining();
+  if (alarmSeconds === 0) {
+    clearInterval(alarmInterval);
+    playAlarm();
+  }
+}
+
+function displayTimeRemaining() {
+  var timeRemainingH1 = document.querySelector("#timeRemaining");
+
+  var minutes = Math.floor(alarmSeconds / 60);
+  var seconds = alarmSeconds - minutes * 60;
+
+  var minutesString = "";
+  if (minutes < 10) {
+    minutesString = "0" + minutes;
+  } else {
+    minutesString = minutes;
+  }
+
+  var secondsString = "";
+  if (seconds < 10) {
+    secondsString = "0" + seconds;
+  } else {
+    secondsString = seconds;
+  }
+
+  timeRemainingH1.innerText =
+    "Time Remaining: " + minutesString + ":" + secondsString;
+}
 
 // DO NOT EDIT BELOW HERE
 
